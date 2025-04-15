4d2f638a9495d8ddcb9130d0f19d4d2143183141
VelvetZZZ <15268589345@163.com>
2025-04-15 22:08:05 +0800

    update

--- cs61a/exercise14.py
+++ cs61a/exercise14.py
@@ -137,10 +137,11 @@ def make_withdraw_list(balance):
      b = [balance]
      def withdraw(amount):
           if amount > b[0]:
-               return 'Insufficient funds'
+             return 'Insufficient funds'
           b[0] = b[0] - amount
-          return withdraw
+          return b[0]
+     return withdraw
           
 withdraw = make_withdraw_list(100)
 
-withdraw(25)
+print(withdraw(25))
