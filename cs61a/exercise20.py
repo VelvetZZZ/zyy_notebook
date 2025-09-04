82aaee4dedcad6f9f4084b1e9a8f038b29d4a205
VelvetZZZ <15268589345@163.com>
2025-09-04 23:44:34 +0800

    upd

--- cs61a/exercise20.py
+++ cs61a/exercise20.py
@@ -2,5 +2,9 @@
 #The repr String for an Object
 print(12e12)
 #The str String for an Object
-
-
+from fractions import Fraction
+half = Fraction(1, 2)
+print(repr(half))
+print(str(half))
+print(half)
+print(type(str(half)))  # 输出：<class 'str'>
