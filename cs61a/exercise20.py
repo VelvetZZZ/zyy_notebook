8a051f69e1f797864476bc7c0c8878c0e3eabbe5
VelvetZZZ <15268589345@163.com>
2025-09-05 00:40:50 +0800

    upd

--- cs61a/exercise20.py
+++ cs61a/exercise20.py
@@ -17,3 +17,28 @@ print(s)
 print(str(s))
 print(repr(s))
 repr(s)#"'Hello, World'"
+
+#F-stings
+#String Interpolation in Python
+#传统（未简化）的形式
+from math import pi
+'pi starts with' + str(pi) + '...'
+
+print('pi starts with' + str(pi) + '...')
+
+#Using sting interpolation(字符串插值)
+f'pi starts with {pi}...'
+print(f'pi starts with {pi}...')
+
+
+print(f'2 + 2 = 2 + 2')
+print(f'2 + 2 = {2 + 2}')
+print('2 + 2 = {2 + 2}')
+
+f'2 + 2 = abs{2 + 2}'
+print(abs)
+abs = float
+print(f'2 + 2 = {abs(2 + 2)}')
+print(f'2 + 2 = {(lambda x: x + x)(2)}')
+
+
