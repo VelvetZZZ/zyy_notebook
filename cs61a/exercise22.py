37c6ab9a241410790b08e2ba40c29a6bc8cf902b
VelvetZZZ <15268589345@163.com>
2025-09-10 20:47:26 +0800

    upd

--- cs61a/exercise22.py
+++ cs61a/exercise22.py
@@ -88,3 +88,38 @@ def square(x):
 
 #Quadratic Time（二次时间复杂度）
 #Space
+
+def factors_fast(n):
+    total = 0
+    sqrt_n = sqrt(n)
+    k = 1
+    while k < sqrt_n:
+        if divides(k, n):
+            total += 2
+        k += 1
+    if k*k == n:
+        total += 1
+    return total
+
+def count_frames(f):
+    def counted(n):
+        counted.open_count += 1
+        if counted.open_count > counted.max_count:
+            counted.max_count = counted.open_count
+        result = f(n)
+        counted.open_count -= 1
+        return result
+    counted.open_count = 0
+    counted.max_count = 0
+    return counted
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-2) + fib(n-1)
+    
+fib = count_frames(fib)
+print(fib(20))
+print(fib.open_count)
+print(fib.max_count)
