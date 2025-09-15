d29d44867d1b952e866ea22a7203ad687771edc3
VelvetZZZ <15268589345@163.com>
2025-09-15 10:43:33 +0800

    upd

--- cs61a/exercise23.py
+++ cs61a/exercise23.py
@@ -62,3 +62,59 @@ def similarity(r1, r2):
     return -abs(r1.stars - r2.stars)  # 星级差距越小，越相似
 
 print(r1.similar(2, similarity))
+
+
+
+
+#设计一个 Restaurant 类，并通过比较评价者（reviewers）来判断两家餐厅的相似性，找出与某家餐厅最相似的 K 家餐厅。
+
+#🧮 相似度函数定义
+def reviewed_both(r1, r2):
+    return len(set(r1.reviewers) & set(r2.reviewers))
+
+#🧱 类定义部分
+class Restaurant:
+    all = []
+
+    def __init__(self, name, stars, reviewers):
+        self.name = name
+        self.stars = stars
+        self.reviewers = reviewers
+        Restaurant.all.append(self)
+
+    def similar(self, k, similarity=reviewed_both):
+        """Return the K most similar restaurants to SELF."""
+        others = Restaurant.all
+        others.remove(self)
+        different = lambda r: -similarity(r, self)
+        return sorted(others, key=different)[:k]
+
+    def __repr__(self):
+        return '<' + self.name + '>'
+    
+#🔁 数据读取部分
+import json
+
+reviewers_for_restaurant = {}
+
+# 读取 reviews.json
+for line in open('reviews.json'):
+    r = json.loads(line)
+    biz = r['business_id']
+    if biz not in reviewers_for_restaurant:
+        reviewers_for_restaurant[biz] = [r['user_id']]
+    else:
+        reviewers_for_restaurant[biz].append(r['user_id'])
+
+# 读取 restaurants.json 并创建 Restaurant 对象
+for line in open('restaurants.json'):
+    r = json.loads(line)
+    reviewers = reviewers_for_restaurant[r['business_id']]
+    Restaurant(r['name'], r['stars'], reviewers)
+
+
+#🔍 餐厅搜索与打印
+results = search('Thai')
+
+for r in results:
+    print(r.name, 'shares reviewers with', r1.similar(3))
