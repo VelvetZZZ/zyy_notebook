4ee486e7ed4ac1c8582bd2901b13d3ba15577b4d
VelvetZZZ <15268589345@163.com>
2025-09-17 19:51:22 +0800

    upd

--- cs61a/exercise24.py
+++ cs61a/exercise24.py
@@ -16,3 +16,37 @@ t.extend(t)
 
 t = [[1, 2], [3, 4]]
 t[0].append(t[1:2])
+
+#Land Owners —— 类属性 vs 实例属性
+
+class Worker:
+    greeting = 'Sir'
+    def __init__(self):
+        self.elf = Worker
+    def work(self):
+        return self.greeting + ', I work'
+    def __repr__(self):
+        return Bourgeoisie.greeting
+
+class Bourgeoisie(Worker):
+    greeting = 'Peon'
+    def work(self):
+        print(Worker.work(self))
+        return 'I gather wealth'
+
+jack = Worker()
+john = Bourgeoisie()
+jack.greeting = 'Maam'
+
+# Examples:Iterables & Iterators
+def min_abs_indices(s):
+    """
+    Indices of all elements in list s that have the smallest absolute value.
+
+    >>> min_abs_indices([-4, -3, -2, 3, 2, 4])
+    [2, 4]
+    >>> min_abs_indices([1, 2, 3, 4, 5])
+    [0]
+    """
+    min_abs = min(abs(x) for x in s)
+    return [i for i in range(len(s)) if abs(s[i]) == min_abs]
