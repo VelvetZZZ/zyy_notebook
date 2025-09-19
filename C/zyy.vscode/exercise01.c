65da847945ac019ca01dbe9f841edaae8095cc7d
VelvetZZZ <15268589345@163.com>
2025-09-19 10:23:29 +0800

    upd

--- C/zyy.vscode/exercise01.c
+++ C/zyy.vscode/exercise01.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+void program1() {
     printf("Hello, C!\n");
+}
+
+void program2() {
+    printf("Hello World!\n");
+}
+
+int main() {
+    // 调用其中一个
+    program1();  // 或者改成 program2();
+    program2();
+    
     return 0;
 }
