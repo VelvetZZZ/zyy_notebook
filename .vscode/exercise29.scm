1ebe556ada41ec4ce8d3c1b75f9f2c8420869f60
VelvetZZZ <15268589345@163.com>
2025-10-10 15:08:57 +0800

    upd

---
+++ .vscode/exercise29.scm
@@ -0,0 +1,20 @@
+(define a 1)
+(define b 2)
+(display(list a b))
+(newline)
+(display "Result: ")
+(display (list a b))
+(newline)
+
+(display(list 'a 'b))
+(newline)
+(display(list 'a b))
+(newline);Quatation
+
+;Quotation can also be applied to combinations to from lists.
+(display'(a, b, c))
+(newline)
+(display(car '(a, b, c)))
+
+
+
