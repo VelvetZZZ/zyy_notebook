d2444a4c8b90dc060741c24072c58ed011c96412
VelvetZZZ <15268589345@163.com>
2025-10-14 23:02:06 +0800

    upd

--- Java/VariableTest.java
+++ Java/VariableTest.java
@@ -6,7 +6,7 @@ public class VariableTest {
         //第一站：上去一位乘客
         count = count + 1;
         //第二站：上去两位乘客， 下来一位乘客
-        count = count + 2 - 1
+        count = count + 2 - 1;
         //第三站：上去两位乘客，下来一位乘客
         count = count + 2 - 1;
         //第四站：下来一位乘客
