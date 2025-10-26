04b21a73452cc918a6f92be94f3d003e30b935c8
VelvetZZZ <15268589345@163.com>
2025-10-27 00:28:05 +0800

    upd

--- .vscode/exercise32.scm
+++ .vscode/exercise32.scm
@@ -3,4 +3,20 @@
 
 (display (+ (square 2) (cube 3)))
 (newline)
+
+(define (factorial n k)
+    (if (zero? n) k
+        (factorial (- n 1)
+                   (* n k))))
+(display (factorial 5 1))
+(newline)
   
+#| python equivalent
+def factorial(n, k):
+    while n > 0:
+        n, k = n-1, k*n
+    return k
+print(factorial(5, 1))
+|#
+
+
