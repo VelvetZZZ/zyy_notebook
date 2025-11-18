09733e52ee27b79c3f52076e0e8b0f7a24b86669
VelvetZZZ <15268589345@163.com>
2025-11-18 21:39:41 +0800

    upd

--- .vscode/exercise32.scm
+++ .vscode/exercise32.scm
@@ -25,13 +25,17 @@ print(factorial(5, 1))
     (if (null? s)
     0
     (+ 1 (length (cdr s)))))
-
+#|不是尾递归|#
 
 (define (length-tail s)
     (define(length-iter s n)
         (if (null? s) n
             (length-iter (cdr s) (+ n 1)))
             length-iter s 0))
+(display (length-tail '(a b c d e f g h i j k l m n o p q r s t u v w x y z)))
+(newline)
+#|尾递归|#
+
 
 
 
