20cde274ffdddce2dba7ce05439c023230cba228
VelvetZZZ <15268589345@163.com>
2025-11-19 16:36:02 +0800

    upd

--- cs61a/__pycache__/.vscode/exercise33.scm
+++ cs61a/__pycache__/.vscode/exercise33.scm
@@ -22,3 +22,40 @@
     (display (fib-exp 6))
 (newline)
     (display(eval (fib-exp 6)))
+(newline)
+
+;Generating Code
+;Quasiquotation 准引用 `,
+;逗号 , 用于取消引用
+;逗号加 at ,@ 用于取消引用并展开列表
+;Example:While Statements
+(begin
+(define (f x total)
+   (if (< x 10)
+    (f (+ x 2) ( + total (* x x))))
+    total))
+(f 2 0)
+(newline)
+
+(begin
+(define(f x total)
+(if (< (* x x) 50)
+ (f (+ x 1) (+ total x))
+ total))
+(f 1 0))
+(display (f 1 0))
+(newline)
+
+
+
+(define (sum-while initial-x condition add-to-total update-x)
+  `(begin
+     (define (f x total)
+       (if ,condition
+           (f ,update-x (+ total ,add-to-total))
+           total))
+     (f ,initial-x 0)))
+(display (eval (sum-while 1 '(< (* x x) 50) 'x '(+ x 1))))
+(newline)
+
+
