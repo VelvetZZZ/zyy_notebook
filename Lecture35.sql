10a01392d8f662916a113bb81c313ed011bdd6d5
VelvetZZZ <15268589345@163.com>
2025-11-25 20:55:58 +0800

    upd

---
+++ Lecture35.sql
@@ -0,0 +1,6 @@
+create table cities as
+  select 38 as latitude, 122 as longitude, "Berkeley" as name union
+  select 42,             71,               "Cambridge"        union
+  select 45,             93,               "Minneapolis";
+
+select * from cities;
