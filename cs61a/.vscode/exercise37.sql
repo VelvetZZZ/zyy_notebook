cd69c49ff7c01d5e2c4273cf177bf5874d8937f0
VelvetZZZ <15268589345@163.com>
2025-12-04 23:56:11 +0800

    upd

--- cs61a/.vscode/exercise37.sql
+++ cs61a/.vscode/exercise37.sql
@@ -1,1 +1,21 @@
-部分展示了如何用 SQL 建立两个基础表格：dogs 和 parents。
+-- 1. 创建动物园 (准备数据)
+DROP TABLE IF EXISTS animals;
+CREATE TABLE animals AS
+  SELECT "dog" AS kind, 4 AS legs, 20 AS weight UNION
+  SELECT "cat"        , 4        , 10           UNION
+  SELECT "ferret"     , 4        , 10           UNION
+  SELECT "parrot"     , 2        , 6            UNION
+  SELECT "penguin"    , 2        , 10           UNION
+  SELECT "t-rex"      , 2        , 12000;
+
+-- 2. PPT 上的例子：谁的腿最多？(Max)
+-- 预期结果: 4
+SELECT max(legs) FROM animals;
+
+-- 3. 玩点新的：所有动物加起来有多重？(Sum)
+-- 预期结果: 12056
+SELECT sum(weight) FROM animals;
+
+-- 4. 玩点新的：这里面有几只动物？(Count)
+-- 预期结果: 6
+SELECT count(*) FROM animals;
