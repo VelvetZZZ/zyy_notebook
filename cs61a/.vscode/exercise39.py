95551d5cb18cd6ad57aec30e441a886052647230
VelvetZZZ <15268589345@163.com>
2025-12-14 20:46:35 +0800

    upd

--- cs61a/.vscode/exercise39.py
+++ cs61a/.vscode/exercise39.py
@@ -1,3 +1,8 @@
-def tree(label, branches=[]):
-    return [label] + list(branches)
+def bigs(t):
+    def f(a, x):  # x = max label among ancestors of a
+        if a.label > x:
+            return 1 + sum(f(b, a.label) for b in a.branches)
+        else:
+            return sum(f(b, x) for b in a.branches)
+    return f(t, t.label - 1)
     
