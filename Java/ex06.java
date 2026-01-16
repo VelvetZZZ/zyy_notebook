3afb64e41d2b871a94e45611d82f9338fca42aea
VelvetZZZ <15268589345@163.com>
2026-01-16 15:24:46 +0800

    upd

---
+++ Java/ex06.java
@@ -0,0 +1,8 @@
+public class ex06 {
+    public static void main(String[] args) {
+        System.out.println(5 << 1);//10
+        System.out.println(5 >> 1);//2
+        System.out.println(5 >>> 1);//2
+        System.out.println(-5 >>> 1);//2147483645
+    }}
+}
