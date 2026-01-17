e84ad6bf54a6449be6575379d7d5e54a0dbef874
VelvetZZZ <15268589345@163.com>
2026-01-17 15:44:57 +0800

    upd

---
+++ Java/ex09.java
@@ -0,0 +1,13 @@
+public class ex09 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 1;
+        while (sum <= 99){
+            sum += num;
+            num++;
+        }
+        System.out.println(sum);
+    }
+}
+    
+
